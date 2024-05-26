a3f7d832bdef631ee33f942b7475484295e72f23
Kaisar <dezexeriboza@gmail.com>
2024-05-26 14:35:09 +0500

    fix bugs

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
 
     package_name='warehouse_bot' #<--- CHANGE ME
     pkg_path = os.path.join(get_package_share_directory('warehouse_bot'))
-    rviz_file = os.path.join(pkg_path,'config','localization.rviz')
+    rviz_file = os.path.join(pkg_path,'config','test2.rviz')
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
